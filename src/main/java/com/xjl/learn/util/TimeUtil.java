e885260fe39234c01324186f372010e9588491e6
xjl <a495753601@163.com>
2021-01-15 16:37:37 +0800

    时间工具修改

--- src/main/java/com/xjl/learn/util/TimeUtil.java
+++ src/main/java/com/xjl/learn/util/TimeUtil.java
@@ -6,4 +6,8 @@ package com.xjl.learn.util;
  * @date: 2021-01-15-16:28
  */
 public class TimeUtil {
+    private final String name = "sss";
+    public String getName(){
+        return name;
+    }
 }
